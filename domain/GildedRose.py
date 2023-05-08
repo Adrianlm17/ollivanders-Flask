b437b4d8cee8bac271034436c36ed1926d2a22d9
Adrián López <adrianlomu2004@gmail.com>
2023-05-08 18:46:57 +0200

    feat(items.html | GildedRose.py | Database.py): implementar eliminar datos tabla.

--- domain/GildedRose.py
+++ domain/GildedRose.py
@@ -1,43 +1,30 @@
 class GildedRose:
-
     def __init__(self, items):
-        
         self.items = items
-        
-    def updateGildedRose(self):
 
+    def updateGildedRose(self):
         for item in self.items:
             item.updateQuality()
 
 
 class Item:
-
     def __init__(self, name, sellIn, quality):
-        
         self.name = name
         self.sellIn = sellIn
         self.quality = quality
 
-
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sellIn, self.quality)
-    
 
 
 class NormalItem(Item):
-
     def __init__(self, name, sellIn, quality):
-
         Item.__init__(self, name, sellIn, quality)
-    
-    
-    def setSellIn(self):
 
+    def setSellIn(self):
         self.sellIn = self.sellIn - 1
-    
 
     def setQuality(self, price):
-
         if self.quality + price > 50:
             self.quality = 50
 
@@ -47,24 +34,19 @@ class NormalItem(Item):
         else:
             self.quality = 0
 
-
     def updateQuality(self):
-
         if self.sellIn > 0:
             self.setQuality(-1)
 
         else:
             self.setQuality(-2)
-            
+
         self.setSellIn()
 
 
-
 class AgedBrie(NormalItem):
-    
     def __init__(self, name, sellIn, quality):
         Item.__init__(self, name, sellIn, quality)
-
 
     def updateQuality(self):
         if self.sellIn > 0:
@@ -75,14 +57,11 @@ class AgedBrie(NormalItem):
         self.setSellIn()
 
 
-
 class BackstagePasses(NormalItem):
-
-    def __init__(self, name, sellIn, quality):   
+    def __init__(self, name, sellIn, quality):
         NormalItem.__init__(self, name, sellIn, quality)
 
     def updateQuality(self):
-
         if self.sellIn < 0:
             self.quality = 0
 
@@ -91,34 +70,27 @@ class BackstagePasses(NormalItem):
 
         elif self.sellIn <= 10:
             self.setQuality(2)
-            
+
         else:
             self.setQuality(1)
 
         self.setSellIn()
 
 
-
 class Conjured(NormalItem):
-
     def __init__(self, name, sellIn, quality):
         NormalItem.__init__(self, name, sellIn, quality)
 
-
     def updateQuality(self):
-        
         if self.sellIn >= 0:
             self.setQuality(-2)
 
         self.setSellIn()
 
 
-
 class Sulfuras(NormalItem):
-
     def __init__(self, name, sellIn, quality):
         Item.__init__(self, name, sellIn, quality)
 
-    
-    def updateQuality(self):    
+    def updateQuality(self):
         pass
