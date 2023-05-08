cbf32baf92fbffcfa122c96b2ace8f2db5338d97
Adrián López <adrianlomu2004@gmail.com>
2023-05-08 18:49:40 +0200

    feat(setup.py): actualizar version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ollivanders",
-    version="0.1",
+    version="1.0",
     author="Adrianlm17",
     author_email="adrianlomu2004@gmail.com",
     description="Proyecto FLASK",
