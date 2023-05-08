ad1831abe9665d4fecaa432f5ed955005928ba28
Adrián López <adrianlomu2004@gmail.com>
2023-05-08 18:50:46 +0200

    hotfix(test_GildedRose.py): modificado espacios con black.

--- test/test_GildedRose.py
+++ test/test_GildedRose.py
@@ -1,6 +1,7 @@
 from domain.GildedRose import *
 import pytest
 
+
 @pytest.fixture
 def gilded_rose():
     items = [
@@ -12,13 +13,13 @@ def gilded_rose():
         BackstagePasses("Backstage passes to a TAFKAL80ETC concert", 15, 20),
         BackstagePasses("Backstage passes to a TAFKAL80ETC concert", 10, 49),
         BackstagePasses("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-        Conjured("Conjured Mana Cake", 3, 6)
+        Conjured("Conjured Mana Cake", 3, 6),
     ]
     return GildedRose(items)
 
 
+# ----------------------- AGED BRIE -----------------------
 
-# ----------------------- AGED BRIE ----------------------- 
 
 @pytest.mark.test_Aged_Brie
 def test_Aged_Brie(gilded_rose):
@@ -33,8 +34,8 @@ def test_Aged_Brie(gilded_rose):
     assert str(gilded_rose.items[1]) == "Aged Brie, -2, 6"
 
 
+# ----------------------- NORMAL ITEM -----------------------
 
-# ----------------------- NORMAL ITEM ----------------------- 
 
 @pytest.mark.test_Elixir
 def test_Elixir(gilded_rose):
@@ -49,8 +50,8 @@ def test_Elixir(gilded_rose):
     assert str(gilded_rose.items[2]) == "Elixir of the Mongoose, 1, 3"
 
 
+# ----------------------- SULFURAS -----------------------
 
-# ----------------------- SULFURAS ----------------------- 
 
 @pytest.mark.test_Sulfuras
 def test_Sulfuras(gilded_rose):
@@ -63,6 +64,7 @@ def test_Sulfuras(gilded_rose):
     assert str(gilded_rose.items[3]) == "Sulfuras, Hand of Ragnaros, 0, 80"
     gilded_rose.updateGildedRose()
     assert str(gilded_rose.items[3]) == "Sulfuras, Hand of Ragnaros, 0, 80"
+
 
 @pytest.mark.test_Sulfuras_dos
 def test_Sulfuras_dos(gilded_rose):
@@ -77,49 +79,81 @@ def test_Sulfuras_dos(gilded_rose):
     assert str(gilded_rose.items[4]) == "Hand of Ragnaros, -1, 80"
 
 
+# ----------------------- BACKSTAGE PASSES -----------------------
 
-# ----------------------- BACKSTAGE PASSES ----------------------- 
 
 @pytest.mark.test_BackstagePasses
 def test_BackstagePasses(gilded_rose):
-    assert str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 15, 20"
+    assert (
+        str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 15, 20"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 14, 21"
+    assert (
+        str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 14, 21"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 13, 22"
+    assert (
+        str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 13, 22"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 12, 23"
+    assert (
+        str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 12, 23"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 11, 24"
+    assert (
+        str(gilded_rose.items[5]) == "Backstage passes to a TAFKAL80ETC concert, 11, 24"
+    )
+
 
 @pytest.mark.test_BackstagePasses_dos
 def test_BackstagePasses_dos(gilded_rose):
-    assert str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 10, 49"
+    assert (
+        str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 10, 49"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 9, 50"
+    assert (
+        str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 9, 50"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 8, 50"
+    assert (
+        str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 8, 50"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 7, 50"
+    assert (
+        str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 7, 50"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 6, 50"
+    assert (
+        str(gilded_rose.items[6]) == "Backstage passes to a TAFKAL80ETC concert, 6, 50"
+    )
+
 
 @pytest.mark.test_BackstagePasses_tres
 def test_BackstagePasses_tres(gilded_rose):
-    assert str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 5, 49"
+    assert (
+        str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 5, 49"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 4, 50"
+    assert (
+        str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 4, 50"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 3, 50"
+    assert (
+        str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 3, 50"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 2, 50"
+    assert (
+        str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 2, 50"
+    )
     gilded_rose.updateGildedRose()
-    assert str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 1, 50"
+    assert (
+        str(gilded_rose.items[7]) == "Backstage passes to a TAFKAL80ETC concert, 1, 50"
+    )
 
 
+# ----------------------- CONJURED -----------------------
 
-# ----------------------- CONJURED ----------------------- 
- 
+
 @pytest.mark.test_Conjured
 def test_Conjured(gilded_rose):
     assert str(gilded_rose.items[8]) == "Conjured Mana Cake, 3, 6"
@@ -131,6 +165,7 @@ def test_Conjured(gilded_rose):
     assert str(gilded_rose.items[8]) == "Conjured Mana Cake, 0, 0"
     gilded_rose.updateGildedRose()
     assert str(gilded_rose.items[8]) == "Conjured Mana Cake, -1, 0"
+
 
 @pytest.mark.test_Conjured_dos
 def test_Conjured_dos(gilded_rose):
